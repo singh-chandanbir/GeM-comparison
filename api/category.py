38d300fe1418eb040dc34b2e965f5d35a9b1b29d
dilrajs616 <dilraj082@gmail.com>
2023-12-15 21:35:42 +0530

    revised code

--- api/category.py
+++ api/category.py
@@ -10,17 +10,19 @@ def getCategory(search_query):
     url = "https://mkp.gem.gov.in/search?q="+search_query
 
     response = requests.get(url)
-    category = []
-    subcategory = []
+    
+    category_dict = {}  # Initialize an empty dictionary to store categories and subcategories
+
     if response.status_code == 200:
         htmlcontent = response.text
         htmlsoup = BS(htmlcontent, 'html.parser') 
         relevant_categories_parent = htmlsoup.find('ul', class_="clearfix")
-        # print(relevant_categories)
-        counter = 0
-        relevant_categories = relevant_categories_parent.find_all('li', class_ = "bn-group")
-        for cat in relevant_categories:
-            category.append(cat.find('strong').text) 
-            subcategory.append([tag.text for tag in cat.find_all('li')])
         
-        return (category,subcategory)
+        relevant_categories = relevant_categories_parent.find_all('li', class_="bn-group")
+        
+        for cat in relevant_categories:
+            category_name = cat.find('strong').text
+            subcategories = [tag.text for tag in cat.find_all('li')]
+            category_dict[category_name] = subcategories
+
+    return category_dict
--- app.py
+++ app.py
@@ -7,15 +7,13 @@ app.secret_key = 'BCQWR#$@@WE@12332423@121'
 #Routes Will be here
 @app.route('/',methods = ["GET","POST"])
 def landing():
-    if request.method == "POST":
-        session["query"] = request.form.get("searchquery")
-        return redirect(url_for('result'))
-    return rt('landing.html')
+    return rt('index.html')
 
 
-@app.route('/result')
+@app.route('/result', methods = ["POST"])
 def result():
-    (category,subcategory) = getCategory(session["query"])
-    return rt('results.html', searchquery = session["query"], category = category, subcategory = subcategory)
+    query = request.form.get("searchquery")
+    category_dict = getCategory(query)
+    return rt('results.html', searchquery = query, category_dict = category_dict)
 if __name__ == '__main__':
     app.run(debug=True)
