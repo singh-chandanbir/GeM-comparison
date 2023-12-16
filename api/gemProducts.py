f7c01347aeb13b655499cce7f1598d1a090910cc
dilrajs616 <dilraj082@gmail.com>
2023-12-16 19:30:50 +0530

    added parallel processing

--- api/gemProducts.py
+++ api/gemProducts.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import requests
 from bs4 import BeautifulSoup as BS
 from api.gemPages import getPages
@@ -20,46 +21,41 @@ def get_product_details(product):
     
     return None
 
-# def get_product_details(product):
-#     details_dict = {}
-#     details_dict['product_title'] = product.find('span', class_='variant-title').text if product.find('span', class_='variant-title') else ''
-#     details_dict['product_brand'] = product.find('div', class_='variant-brand').text.replace('Brand:', '') if product.find('div', class_='variant-brand') else ''
-#     details_dict['product_min_qty'] = product.find('div', class_='variant-moq').text.replace('Min. Qty. Per Consignee:', '') if product.find('div', class_='variant-moq') else ''
-#     details_dict['product_final_price'] = product.find('span', class_='variant-list-price').text if product.find('span', class_='variant-list-price') else ''
-#     details_dict['product_list_price'] = product.find('span', class_='variant-final-price').text if product.find('span', class_='variant-final-price') else ''
-#     return details_dict
+def process_page(link, page_number):
+    url = f"https://mkp.gem.gov.in{link}&page={page_number}"
+    content = fetch_page_content(url)
+    html_soup = BS(content, 'html.parser') if content else None
+    if html_soup.find('div', id='no-results-found'):
+        return []
+    
+    product_detail_list = []
+    product_list = html_soup.find_all('li', class_="clearfix")
+    for product in product_list:
+        details_dict = get_product_details(product)
+        if details_dict is not None:
+            product_detail_list.append(details_dict)
+    return product_detail_list
 
 def get_products(query):
     product_detail_list = []
     product_pages = getPages(query)
     
-    for link in product_pages:
-        base_url = f"https://mkp.gem.gov.in{link}&page="
-        page_number = 1
-        highest_page_number = 1
-        initial_url = f"https://mkp.gem.gov.in{link}"
-        initial_content = fetch_page_content(initial_url)
-        
-        initial_soup = BS(initial_content, 'html.parser')
-        pagination_div = initial_soup.find('div', class_='pagination')
-        if pagination_div:
-            page_links = pagination_div.find_all('a')
-            page_numbers = [int(link['href'].split('=')[-1]) for link in page_links]
-            highest_page_number = max(page_numbers)
-                
-        while (page_number <= highest_page_number):
-            url = f"https://mkp.gem.gov.in{link}&page={page_number}"
-            content = fetch_page_content(url)
-            html_soup = BS(content, 'html.parser') if content else None
-            if html_soup.find('div', id='no-results-found'):
-                break
-            
-            product_list = html_soup.find_all('li', class_="clearfix")
-            for product in product_list:
-                details_dict = get_product_details(product)
-                if details_dict is not None:
-                    product_detail_list.append(details_dict)
-            
-            page_number += 1
-    
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for link in product_pages:
+            initial_url = f"https://mkp.gem.gov.in{link}"
+            initial_content = fetch_page_content(initial_url)
+            initial_soup = BS(initial_content, 'html.parser')
+            pagination_div = initial_soup.find('div', class_='pagination')
+            if pagination_div:
+                page_links = pagination_div.find_all('a')
+                page_numbers = [int(link['href'].split('=')[-1]) for link in page_links]
+                highest_page_number = max(page_numbers)
+
+                for page_number in range(1, highest_page_number + 1):
+                    futures.append(executor.submit(process_page, link, page_number))
+
+        for future in concurrent.futures.as_completed(futures):
+            product_detail_list.extend(future.result())
+
     return product_detail_list
